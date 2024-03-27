0ccdfff15c4f94b43df752a4729d8938b28df84f
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-27 22:58:59 +0400

    updated github action to setup direnv instead of waiting, added new dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,36 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as tc from '@actions/tool-cache'
+import * as exec from '@actions/exec'
+import * as io from '@actions/io'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const version: string = core.getInput('version')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const cache = tc.find('direnv', version)
+    if (cache) {
+      core.addPath(cache)
+      return
+    }
+    const url = `https://github.com/direnv/direnv/releases/download/v${version}/direnv.linux-amd64`
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // Download the binary
+    const downloadPath = await tc.downloadTool(url)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Make the binary executable
+    await exec.exec(`chmod +x ${downloadPath}`)
+
+    // Add the binary to the tool cache
+    const cachedPath = await tc.cacheFile(
+      downloadPath,
+      'direnv',
+      'direnv',
+      version
+    )
+
+    // Add the binary to the PATH
+    core.addPath(cachedPath)
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
