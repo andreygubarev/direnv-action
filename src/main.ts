00b9aa52d1ae294e2dbd2a474aa8cc6854cf26f0
Andrey Gubarev <andrey@andreygubarev.com>
2024-03-27 23:16:38 +0400

    added platform and architecture support to the tool caching and download url in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
 import * as core from '@actions/core'
 import * as tc from '@actions/tool-cache'
 import * as exec from '@actions/exec'
+import { arch, platform } from 'process'
+
 
 export async function run(): Promise<void> {
   try {
     const version: string = core.getInput('version')
+    const architecture = arch === 'x64' ? 'amd64' : arch
+    switch (platform) {
+      case 'linux':
+      case 'darwin':
+        break
+      default:
+        throw new Error(`Unsupported platform: ${platform}`)
+    }
 
-    const cache = tc.find('direnv', version)
+    const cache = tc.find('direnv', `${version}-${platform}-${architecture}`)
     if (cache) {
       core.addPath(cache)
       return
     }
-    const url = `https://github.com/direnv/direnv/releases/download/v${version}/direnv.linux-amd64`
+    const url = `https://github.com/direnv/direnv/releases/download/v${version}/direnv.${platform}-${architecture}`
 
     // Download the binary
     const downloadPath = await tc.downloadTool(url)
